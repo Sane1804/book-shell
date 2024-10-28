0d04f4bf498e518bd1fe0913d6df85d573eb6da9
Alexander Vielma <vielmaa1804@gmail.com>
2024-10-28 10:35:42 -0400

    Created function that adds books to the library

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const SUBMIT = document.querySelector("#submit");
 const FORM = document.querySelector(".form")
+const BOOKS_CONTAINER = document.querySelector(".books-container");
 
 let myLibrary = [{title: "The happy death", author: "Sane", pages: 250, read: true}];
 
@@ -9,11 +10,35 @@ const createBookCard = () => {
 }
 
 const displayBooks = (books) => {
+    BOOKS_CONTAINER.innerHTML = "";
     let library = books;
     for(let i = 0; i < library.length; i++){
+        let card = document.createElement("div");
+        card.className = "book-card"
+        let title = document.createElement("h3");
+        let author = document.createElement("p");
+        let pages = document.createElement("p");
+        let read = document.createElement("p");    
         for(let key in library[i]){
-            console.log(library[i][key])
+            
+            if (key == "title"){
+                let textContent = document.createTextNode(`${library[i][key]}`);
+                title.appendChild(textContent)
+            } else if (key == "author") {
+                let textContent = document.createTextNode(`${library[i][key]}`)
+                author.appendChild(textContent);
+            } else if (key === "pages"){
+                let textContent = document.createTextNode(`${library[i][key]}`)
+                pages.appendChild(textContent);
+            } else {
+                let textContent = document.createTextNode(`${library[i][key]}`)
+                read.appendChild(textContent);
+            }
+
         }
+        card.append(title, author, pages, read);
+        BOOKS_CONTAINER.appendChild(card);
+        console.log(card)
     }
 }
 displayBooks(myLibrary)
@@ -38,6 +63,7 @@ const addBookToLibrary = () => {
        }
         arr.push(FORM.elements[i].value)
     }
+
     let newBook = new Book(...arr);
     myLibrary.push(newBook);
 }
